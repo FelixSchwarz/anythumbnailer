813e72e88fad1d648f137725d6cc5f5151a70f4f
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2014-04-14 00:08:20 +0200

    disable thumbnailers which don't have the necessary binaries installed. Also it's possible (with ugly process-wide monkey-patching) to change an executable path.

--- anythumbnailer/thumbnail_.py
+++ anythumbnailer/thumbnail_.py
@@ -1,54 +1,106 @@
 
 from __future__ import absolute_import
 
+import os
 import mimetypes
 
 from .sh_utils import pipe_with_input, run
 
 __all__ = ['create_thumbnail']
 
+
 def create_thumbnail(source_filename, dimensions=None, **kwargs):
     assert dimensions is None
     mime_type, encoding = mimetypes.guess_type(source_filename, strict=False)
-    thumbnailer = thumbnailers.get(mime_type)
-    if thumbnailers[mime_type] is None:
+    thumbnailer = thumbnailer_for(mime_type)
+    if thumbnailer is None:
         return None
-    return thumbnailer(source_filename, dimensions, **kwargs)
+    return thumbnailer.thumbnail(source_filename, dimensions, **kwargs)
 
-def poppler_thumbnail(source_filename_or_fp, dimensions=None, page=1, output_format='jpg'):
-    assert dimensions is None
-    
-    pdftoppm_args = (
-        '/usr/bin/pdftoppm',
-            '-scale-to', str(2048),
-            '-f', str(page),
-            '-l', str(page))
-    pnm_converter = pnm_converters[output_format]
-    pnm_converter_args = (pnm_converter, )
-    thumbnail = pipe_with_input(source_filename_or_fp, pdftoppm_args, pnm_converter_args)
-    return thumbnail
 
-def unoconv_thumbnail(source_filename, dimensions=None, output_format='jpg'):
-    assert dimensions is None
-    args = (
-        '/usr/bin/unoconv',
-        '-f', 'pdf',
-        '--stdout',
-        source_filename
-    )
-    pdf_fp = run(args)
-    pdf_thumbnailer = thumbnailers['application/pdf']
-    return pdf_thumbnailer(pdf_fp, dimensions=dimensions, output_format=output_format)
+class Thumbnailer(object):
+    def is_available(self):
+        if hasattr(self, 'executables'):
+            executables = self.executables
+        else:
+            executables = (self.executable, )
+        for command_path in executables:
+            command = command_path.split(' ', 1)[0]
+            if not os.path.exists(command):
+                return False
+        return True
 
-pnm_converters = {
-    'jpg': '/usr/bin/pnmtojpeg',
-    'png': '/usr/bin/pnmtopng',
-}
+    def thumbnail(self, source_filename_or_fp, **kwargs):
+        raise NotImplementedError()
+
+
+class PNMToImage(Thumbnailer):
+    pnm_to_png = '/usr/bin/pnmtopng'
+    pnm_to_jpg = '/usr/bin/pnmtojpeg'
+    executables = (pnm_to_png, pnm_to_jpg)
+
+    def pipe_args(self, dimensions=None, output_format='jpg'):
+        assert dimensions is None
+        executable = self.pnm_to_jpg if (output_format == 'jpg') else self.pnm_to_png
+        return (
+            executable,
+        )
+
+    def thumbnail(self, source_filename_or_fp, **kwargs):
+        return run(self.pipe_args(**kwargs), input_=source_filename_or_fp)
+
+
+class Poppler(Thumbnailer):
+    pdf_to_ppm = '/usr/bin/pdftoppm'
+    executables = (pdf_to_ppm, ) + PNMToImage.executables
+
+    def _args(self, dimensions=None, page=1):
+        assert dimensions is None
+        return (
+            self.pdf_to_ppm,
+                '-scale-to', str(2048),
+                '-f', str(page),
+                '-l', str(page)
+        )
+
+    def thumbnail(self, source_filename_or_fp, dimensions=None, page=1, output_format='jpg'):
+        assert dimensions is None
+        pdftoppm_args = self._args(dimensions=dimensions, page=page)
+        pnm_converter_args = PNMToImage().pipe_args(dimensions=dimensions, output_format=output_format)
+        thumbnail = pipe_with_input(source_filename_or_fp, pdftoppm_args, pnm_converter_args)
+        return thumbnail
+
+
+class Unoconv(Thumbnailer):
+    executable = '/usr/bin/unoconv'
+
+    def _args(self, source_filename):
+        return (
+            self.executable,
+            '-f', 'pdf',
+            '--stdout',
+            source_filename
+        )
+
+    def thumbnail(self, source_filename_or_fp, dimensions=None, page=1, output_format='jpg'):
+        pdf_fp = run(self._args(source_filename_or_fp))
+        pdf_thumbnailer = thumbnailer_for('application/pdf')
+        return pdf_thumbnailer.thumbnail(pdf_fp, dimensions=dimensions,
+            page=page, output_format=output_format)
+
 
 thumbnailers = {
-    'application/pdf': poppler_thumbnail,
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document': unoconv_thumbnail, # docx
-    'application/vnd.openxmlformats-officedocument.presentationml.presentation': unoconv_thumbnail, # pptx
-    'application/msword': unoconv_thumbnail, # doc
-    'application/vnd.ms-powerpoint': unoconv_thumbnail, # ppt
+    'image/x-portable-pixmap': PNMToImage,
+    'application/pdf': Poppler,
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document': Unoconv, # docx
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation': Unoconv, # pptx
+    'application/msword': Unoconv, # doc
+    'application/vnd.ms-powerpoint': Unoconv, # ppt
 }
+
+def thumbnailer_for(mime_type):
+    thumbnailer = thumbnailers.get(mime_type)
+    if (thumbnailer is None) or (not thumbnailer().is_available()):
+        return None
+    return thumbnailer()
+
