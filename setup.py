deccb48c7bebfa8872032088a5686dc9682b4afd
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2016-01-20 22:48:08 +0100

    bump version to 0.1, add some description+url to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
+import os
+
 from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(
     name='anythumbnailer',
+    version='0.1',
     description='Generic Thumbnailer library',
-    version='0.1dev',
+    long_description=read('README.txt'),
 
     author='Felix Schwarz',
     author_email='info@schwarz.eu',
+    url='https://github.com/FelixSchwarz/anythumbnailer',
     license='MIT', # see LICENSE.txt
     packages=find_packages(),
     entry_points = {
